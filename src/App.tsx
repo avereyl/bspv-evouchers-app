9402211aa4d1f8532c13118aa0c4cd881015f47c
Guillaume Billaud <guillaume.billaud@dachser.com>
2019-06-14 16:17:08 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import MainMenu from './components/MainMenu';
+import {CssBaseline} from "@material-ui/core";
+
 import './App.css';
 
 const App: React.FC = () => {
+  
   return (
+    <React.Fragment>
+    <CssBaseline />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+
+      <MainMenu></MainMenu>
     </div>
+    </React.Fragment>
   );
 }
 
